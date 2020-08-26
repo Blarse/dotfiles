cadb9c835d3a99cef1d2d92a5d96e40247fcfa35
Egor Ignatov <master.egor.ignatov@gmail.com>
2020-08-26 23:20:41 +0300

    Before I started to mess with evil mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-	(ggtags magit swiper ivy helm nasm-mode smex expand-region org-bullets ace-window htmlize which-key try use-package))))
+   '(evil evil-mode ggtags magit swiper ivy helm nasm-mode smex expand-region org-bullets ace-window htmlize which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,11 +47,11 @@
 ;;(use-package swiper
 ;;	:ensure t)
 
-(use-package helm
-	:ensure t)
-
-;;(use-package evil-mode
+;;(use-package helm
 ;;	:ensure t)
+
+(use-package evil
+	:ensure t)
 
 (use-package try
 	:ensure t)
@@ -98,6 +97,10 @@
 
 ;; Ace Window
 (global-set-key [remap other-window] 'ace-window)
+
+;; evil
+;; (evil-mode 1)
+
 
 ;; ================
 ;;	Emacs Settings
